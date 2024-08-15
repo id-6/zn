8d40895b81cdc4de6e78f98adc5d0d66c22175b0
ابراهيم <143281628+id-6@users.noreply.github.com>
2024-08-15 22:08:23 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ class Config:
     API_ID = os.environ.get("API_ID", "18421930")
     API_HASH = os.environ.get("API_HASH", "9cf3a6feb6dfcc7c02c69eb2c286830e")
     TOKEN = os.environ.get("TOKEN", "5969199738:AAEejKnKJU_9bZj4vJZZf6PzR9nwZxIgm6s")
-    START_PIC = os.environ.get("START_PIC", "ut")
+    START_PIC = os.environ.get("START_PIC", "https://telegra.ph/file/9c9d5d49b28c28fd25f1b.jpg")
     CHAT = os.environ.get("CHAT","ty")
 
 
